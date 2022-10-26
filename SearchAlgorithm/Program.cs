134f82e32ad6c18d3cdc1c573fa84e98f8cc0e01
TA1069 <maylafaat@gmail.com>
2022-10-26 16:12:01 +0700

    add function

---
+++ SearchAlgorithm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //number of elements in the array
+        int n;
+        //get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
